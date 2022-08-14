22627991dfd1e33a3ae38b191e22ffda65ca2781
CodingShivam27 <codingshivam27@gmail.com>
2022-08-14 18:06:34 +0530

    VaccineNotFound exception added

---
+++ src/main/java/com/cowin/exceptions/VaccineNotFound.java
@@ -0,0 +1,14 @@
+package com.cowin.exceptions;
+
+public class VaccineNotFound extends RuntimeException{
+
+	public VaccineNotFound() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public VaccineNotFound(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+	
+}
